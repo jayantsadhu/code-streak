ca471bbd55e51c6d89f952702f9d6d0abb379067
Jayant Kumar Sadhu <69089742+jayantsadhu@users.noreply.github.com>
2022-04-24 19:05:02 +0530

    Time: 2 ms (83.83%), Space: 48.8 MB (49.33%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -3,11 +3,7 @@ class Solution {
         int n = mat.length;
         int arr[][] = new int[n][2];
         for(int i=0 ; i<n ; i++){
-            int j = 0;
-            while(j<mat[0].length && mat[i][j]==1){
-                arr[i][0]++;
-                j++;
-            }
+            arr[i][0] = numOfOnes(mat[i]);
             arr[i][1] = i;
         }
         Arrays.sort(arr, (a, b) -> (a[0]-b[0]));
@@ -16,5 +12,20 @@ class Solution {
             res[i] = arr[i][1];
         }
         return res;
+    }
+    public int numOfOnes(int arr[]){
+        int l = 0;
+        int r = arr.length-1;
+        int count = 0;
+        while(l<=r){
+            int m = l + (r-l)/2;
+            if(arr[m]==1){
+                count = m+1;
+                l = m+1;
+            }
+            else
+                r = m-1;
+        }
+        return count;
     }
 }
