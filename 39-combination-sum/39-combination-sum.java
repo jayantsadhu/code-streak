9977fd4356e58410f2905784c57754a67fd41f35
Jayant Kumar Sadhu <69089742+jayantsadhu@users.noreply.github.com>
2022-06-22 00:20:11 +0530

    Time: 9 ms (31.82%), Space: 45 MB (49.05%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,20 +1,20 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res = new ArrayList();
-        helper(candidates, 0, target, new ArrayList(), res);
+        List<List<Integer>> res = new ArrayList<>();
+        solve(candidates, 0, 0, target, new ArrayList<>(), res);
         return res;
     }
-    public void helper(int[] c, int idx, int target, List<Integer> path, List<List<Integer>> res){
-        if(target==0){
-            res.add(new ArrayList(path));
+    public void solve(int[] c, int i, int s, int t, List<Integer> list,
+                      List<List<Integer>> res){
+        if(i>=c.length || s>t)
+            return;
+        if(s==t){
+            res.add(new ArrayList(list));
             return;
         }
-        for(int i=idx ; i<c.length ; i++) {
-            if(c[i]<=target){
-                path.add(c[i]);
-                helper(c, i, target-c[i], path, res);
-                path.remove(path.size()-1);
-            }
-        }
+        list.add(c[i]);
+        solve(c, i, s+c[i], t, list, res);
+        list.remove(list.size()-1);
+        solve(c, i+1, s, t, list, res); 
     }
 }
