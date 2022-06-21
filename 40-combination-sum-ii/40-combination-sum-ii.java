258aab82ef3c838d81e3e6c6fc73e4c0cfbaaf12
Jayant Kumar Sadhu <69089742+jayantsadhu@users.noreply.github.com>
2022-06-22 01:21:29 +0530

    Time: 6 ms (55.99%), Space: 44.3 MB (22.58%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.java
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -1,33 +1,27 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        int m[] = new int[51];
+        for(Integer num : candidates)
+            m[num]++;
         List<List<Integer>> res = new ArrayList<>();
-        Map<Integer, Integer> map = new HashMap<>();
-        for(Integer val : candidates)
-            map.put(val, map.getOrDefault(val, 0)+1);
-        int c[][] = new int[map.size()][2];
-        int i = 0;
-        for(Integer key : map.keySet()){
-            c[i][0] = key;
-            c[i++][1] = map.get(key);
-        }
-        getCombinations(c, 0, target, new ArrayList<>(), res);
+        solve(m, 1, target, new ArrayList<>(), res);
         return res;
     }
-    public void getCombinations(int c[][], int idx, int target, List<Integer> path, 
-                                                            List<List<Integer>> res){
-        if(target==0){
+    public void solve(int[] m, int i, int t, List<Integer> path, 
+                      List<List<Integer>> res)
+    {
+        if(t<0) return;
+        if(t==0){
             res.add(new ArrayList<>(path));
             return;
         }
-        for(int i=idx ; i<c.length ; i++){
-            if(c[i][1]==0) continue;
-            if(c[i][0]<=target){
-                path.add(c[i][0]);
-                c[i][1] -= 1;
-                getCombinations(c, i, target-c[i][0], path, res);
-                c[i][1] += 1;
-                path.remove(path.size()-1);
-            }
+        for(int idx=i ;  idx<=50 ; idx++){
+            if(m[idx]==0) continue;
+            m[idx]--;
+            path.add(idx);
+            solve(m, idx, t-idx, path, res);
+            m[idx]++;
+            path.remove(path.size()-1);
         }
     }
 }
