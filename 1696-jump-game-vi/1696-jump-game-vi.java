ffdf092de589d4ae88e975d13c61097e0d17cbf4
Jayant Kumar Sadhu <69089742+jayantsadhu@users.noreply.github.com>
2022-07-10 04:31:51 +0530

    Time: 50 ms (60.16%), Space: 117.3 MB (12.25%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.java
+++ 1696-jump-game-vi/1696-jump-game-vi.java
@@ -3,23 +3,20 @@ class Solution {
     public int maxResult(int[] nums, int k) {
         int n = nums.length;
         int[] dp = new int[n];
-        SortedMap<Integer, Integer> set = new TreeMap<>(Collections.reverseOrder());
-        set.put(nums[n-1], 1);
-        dp[n-1] = nums[n-1];
-        for(int i=n-2 ; i>=0 ; i--){
-            if(i>=n-k){
-                dp[i] = set.firstKey() + nums[i];
-                set.put(dp[i], set.getOrDefault(dp[i], 0)+1);
-            }
-            else {
-                dp[i] = nums[i] + set.firstKey();
-                if(set.get(dp[i+k])>1)
-                    set.put(dp[i+k], set.get(dp[i+k])-1);
-                else set.remove(dp[i+k]);
-                set.put(dp[i], set.getOrDefault(dp[i], 0)+1);
-                
-            }
+        
+        Deque<Integer> dq = new LinkedList<>();
+        
+        dp[0] = nums[0];
+        dq.add(0);
+        for(int i=1 ; i<n ; i++){
+            if(dq.peekFirst()<i-k) 
+                dq.pollFirst();
+            dp[i] = dp[dq.peekFirst()] + nums[i];
+            while(!dq.isEmpty() && dp[dq.peekLast()]<=dp[i])
+                dq.pollLast();
+            dq.add(i);
+            
         }
-        return dp[0];
+        return dp[n-1];
     }
 }
